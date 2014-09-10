eeadfe4a4f45fe5602c9ea4819052b129f077ed4
Huge-IT <Huge-IT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 09:15:03 +0000

    Add video gallery plugin
    git-svn-id: https://plugins.svn.wordpress.org/gallery-video/trunk@986224 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/video_gallery_func.php
@@ -0,0 +1,466 @@
+	<?php	
+	if(function_exists('current_user_can'))
+	if(!current_user_can('manage_options')) {
+	die('Access Denied');
+}	
+if(!function_exists('current_user_can')){
+	die('Access Denied');
+}
+
+function showvideogallery() 
+  {
+	  
+  global $wpdb;
+  
+  if(isset($_POST['search_events_by_title']))
+$_POST['search_events_by_title']=esc_html(stripslashes($_POST['search_events_by_title']));
+if(isset($_POST['asc_or_desc']))
+$_POST['asc_or_desc']=esc_js($_POST['asc_or_desc']);
+if(isset($_POST['order_by']))
+$_POST['order_by']=esc_js($_POST['order_by']);
+  $where='';
+  	$sort["custom_style"] ="manage-column column-autor sortable desc";
+	$sort["default_style"]="manage-column column-autor sortable desc";
+	$sort["sortid_by"]='id';
+	$sort["1_or_2"]=1;
+	$order='';
+	
+	if(isset($_POST['page_number']))
+	{
+			
+			if($_POST['asc_or_desc'])
+			{
+				$sort["sortid_by"]=$_POST['order_by'];
+				if($_POST['asc_or_desc']==1)
+				{
+					$sort["custom_style"]="manage-column column-title sorted asc";
+					$sort["1_or_2"]="2";
+					$order="ORDER BY ".$sort["sortid_by"]." ASC";
+				}
+				else
+				{
+					$sort["custom_style"]="manage-column column-title sorted desc";
+					$sort["1_or_2"]="1";
+					$order="ORDER BY ".$sort["sortid_by"]." DESC";
+				}
+			}
+	if($_POST['page_number'])
+		{
+			$limit=($_POST['page_number']-1)*20; 
+		}
+		else
+		{
+			$limit=0;
+		}
+	}
+	else
+		{
+			$limit=0;
+		}
+	if(isset($_POST['search_events_by_title'])){
+		$search_tag=esc_html(stripslashes($_POST['search_events_by_title']));
+		}
+		
+		else
+		{
+		$search_tag="";
+		}		
+		
+	 if(isset($_GET["catid"])){
+	    $cat_id=$_GET["catid"];	
+		}
+       else
+	   {
+       if(isset($_POST['cat_search'])){
+		$cat_id=$_POST['cat_search'];
+		}else{
+		
+		$cat_id=0;}
+       }
+     
+ if ( $search_tag ) {
+		$where= " WHERE name LIKE '%".$search_tag."%' ";
+	}
+if($where){
+	  if($cat_id){
+	  $where.=" AND sl_width=" .$cat_id;
+	  }
+	
+	}
+	else{
+	if($cat_id){
+	  $where.=" WHERE sl_width=" .$cat_id;
+	  }
+	
+	}
+	
+	 $cat_row_query="SELECT id,name FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE sl_width=0";
+	$cat_row=$wpdb->get_results($cat_row_query);
+	
+	// get the total number of records
+	$query = "SELECT COUNT(*) FROM ".$wpdb->prefix."huge_it_videogallery_galleries". $where;
+	
+	$total = $wpdb->get_var($query);
+	$pageNav['total'] =$total;
+	$pageNav['limit'] =	 $limit/20+1;
+	
+	if($cat_id){
+	$query ="SELECT  a.* ,  COUNT(b.id) AS count, g.par_name AS par_name FROM ".$wpdb->prefix."huge_it_videogallery_galleries  AS a LEFT JOIN ".$wpdb->prefix."huge_it_videogallery_galleries AS b ON a.id = b.sl_width LEFT JOIN (SELECT  ".$wpdb->prefix."huge_it_videogallery_galleries.ordering as ordering,".$wpdb->prefix."huge_it_videogallery_galleries.id AS id, COUNT( ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id ) AS prod_count
+FROM ".$wpdb->prefix."huge_it_videogallery_videos, ".$wpdb->prefix."huge_it_videogallery_galleries
+WHERE ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id = ".$wpdb->prefix."huge_it_videogallery_galleries.id
+GROUP BY ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id) AS c ON c.id = a.id LEFT JOIN
+(SELECT ".$wpdb->prefix."huge_it_videogallery_galleries.name AS par_name,".$wpdb->prefix."huge_it_videogallery_galleries.id FROM ".$wpdb->prefix."huge_it_videogallery_galleries) AS g
+ ON a.sl_width=g.id WHERE  a.name LIKE '%".$search_tag."%' group by a.id ". $order ." "." LIMIT ".$limit.",20" ; 
+
+	 }
+	 else{
+	 $query ="SELECT  a.* ,  COUNT(b.id) AS count, g.par_name AS par_name FROM ".$wpdb->prefix."huge_it_videogallery_galleries  AS a LEFT JOIN ".$wpdb->prefix."huge_it_videogallery_galleries AS b ON a.id = b.sl_width LEFT JOIN (SELECT  ".$wpdb->prefix."huge_it_videogallery_galleries.ordering as ordering,".$wpdb->prefix."huge_it_videogallery_galleries.id AS id, COUNT( ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id ) AS prod_count
+FROM ".$wpdb->prefix."huge_it_videogallery_videos, ".$wpdb->prefix."huge_it_videogallery_galleries
+WHERE ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id = ".$wpdb->prefix."huge_it_videogallery_galleries.id
+GROUP BY ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id) AS c ON c.id = a.id LEFT JOIN
+(SELECT ".$wpdb->prefix."huge_it_videogallery_galleries.name AS par_name,".$wpdb->prefix."huge_it_videogallery_galleries.id FROM ".$wpdb->prefix."huge_it_videogallery_galleries) AS g
+ ON a.sl_width=g.id WHERE a.name LIKE '%".$search_tag."%'  group by a.id ". $order ." "." LIMIT ".$limit.",20" ; 
+}
+
+$rows = $wpdb->get_results($query);
+ global $glob_ordering_in_cat;
+if(isset($sort["sortid_by"]))
+{
+	if($sort["sortid_by"]=='ordering'){
+	if($_POST['asc_or_desc']==1){
+		$glob_ordering_in_cat=" ORDER BY ordering ASC";
+	}
+	else{
+		$glob_ordering_in_cat=" ORDER BY ordering DESC";
+	}
+	}
+}
+$rows=open_cat_in_tree($rows);
+	$query ="SELECT  ".$wpdb->prefix."huge_it_videogallery_galleries.ordering,".$wpdb->prefix."huge_it_videogallery_galleries.id, COUNT( ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id ) AS prod_count
+FROM ".$wpdb->prefix."huge_it_videogallery_videos, ".$wpdb->prefix."huge_it_videogallery_galleries
+WHERE ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id = ".$wpdb->prefix."huge_it_videogallery_galleries.id
+GROUP BY ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id " ;
+	$prod_rows = $wpdb->get_results($query);
+		
+foreach($rows as $row)
+{
+	foreach($prod_rows as $row_1)
+	{
+		if ($row->id == $row_1->id)
+		{
+			$row->ordering = $row_1->ordering;
+		$row->prod_count = $row_1->prod_count;
+	}
+		}
+	
+	}
+	
+
+	 
+
+	 
+	$cat_row=open_cat_in_tree($cat_row);
+		html_showvideogallerys( $rows, $pageNav,$sort,$cat_row);
+  }
+
+function open_cat_in_tree($catt,$tree_problem='',$hihiih=1){
+
+global $wpdb;
+global $glob_ordering_in_cat;
+static $trr_cat=array();
+if(!isset($search_tag))
+$search_tag='';
+if($hihiih)
+$trr_cat=array();
+foreach($catt as $local_cat){
+	$local_cat->name=$tree_problem.$local_cat->name;
+	array_push($trr_cat,$local_cat);
+	$new_cat_query=	"SELECT  a.* ,  COUNT(b.id) AS count, g.par_name AS par_name FROM ".$wpdb->prefix."huge_it_videogallery_galleries  AS a LEFT JOIN ".$wpdb->prefix."huge_it_videogallery_galleries AS b ON a.id = b.sl_width LEFT JOIN (SELECT  ".$wpdb->prefix."huge_it_videogallery_galleries.ordering as ordering,".$wpdb->prefix."huge_it_videogallery_galleries.id AS id, COUNT( ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id ) AS prod_count
+FROM ".$wpdb->prefix."huge_it_videogallery_videos, ".$wpdb->prefix."huge_it_videogallery_galleries
+WHERE ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id = ".$wpdb->prefix."huge_it_videogallery_galleries.id
+GROUP BY ".$wpdb->prefix."huge_it_videogallery_videos.videogallery_id) AS c ON c.id = a.id LEFT JOIN
+(SELECT ".$wpdb->prefix."huge_it_videogallery_galleries.name AS par_name,".$wpdb->prefix."huge_it_videogallery_galleries.id FROM ".$wpdb->prefix."huge_it_videogallery_galleries) AS g
+ ON a.sl_width=g.id WHERE a.name LIKE '%".$search_tag."%' AND a.sl_width=".$local_cat->id." group by a.id  ".$glob_ordering_in_cat; 
+ $new_cat=$wpdb->get_results($new_cat_query);
+ open_cat_in_tree($new_cat,$tree_problem. "— ",0);
+}
+return $trr_cat;
+
+}
+
+function editvideogallery($id)
+  {
+	  
+	  global $wpdb;
+	  
+	  if(isset($_GET["removeslide"])){
+	     if($_GET["removeslide"] != ''){
+	
+
+	  $wpdb->query("DELETE FROM ".$wpdb->prefix."huge_it_videogallery_videos  WHERE id = ".$_GET["removeslide"]." ");
+
+
+	
+	   }
+	   }
+
+	   $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id= %d",$id);
+	   $row=$wpdb->get_row($query);
+	   if(!isset($row->videogallery_list_effects_s))
+	   return 'id not found';
+       $images=explode(";;;",$row->videogallery_list_effects_s);
+	   $par=explode('	',$row->param);
+	   $count_ord=count($images);
+	   $cat_row=$wpdb->get_results("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id!=" .$id." and sl_width=0");
+       $cat_row=open_cat_in_tree($cat_row);
+	   	  $query=$wpdb->prepare("SELECT name,ordering FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE sl_width=%d  ORDER BY `ordering` ",$row->sl_width);
+	   $ord_elem=$wpdb->get_results($query);
+	   
+	    $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_videos where videogallery_id = %d order by ordering ASC  ",$row->id);
+			   $rowim=$wpdb->get_results($query);
+			   
+			   if(isset($_GET["addslide"])){
+			   if($_GET["addslide"] == 1){
+	
+$table_name = $wpdb->prefix . "huge_it_videogallery_videos";
+    $sql_2 = "
+INSERT INTO 
+
+`" . $table_name . "` ( `name`, `videogallery_id`, `description`, `image_url`, `sl_url`, `ordering`, `published`, `published_in_sl_width`) VALUES
+( '', '".$row->id."', '', '', '', 'par_TV', 2, '1' )";
+
+    $wpdb->query($sql_huge_it_videogallery_videos);
+	
+
+      $wpdb->query($sql_2);
+	
+	   }
+	   }
+	   
+	
+	   
+	   $query="SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries order by id ASC";
+			   $rowsld=$wpdb->get_results($query);
+			  
+			    $query = "SELECT *  from " . $wpdb->prefix . "huge_it_videogallery_params ";
+
+    $rowspar = $wpdb->get_results($query);
+
+    $paramssld = array();
+    foreach ($rowspar as $rowpar) {
+        $key = $rowpar->name;
+        $value = $rowpar->value;
+        $paramssld[$key] = $value;
+    }
+	
+	 $query="SELECT * FROM ".$wpdb->prefix."posts where post_type = 'post' and post_status = 'publish' order by id ASC";
+			   $rowsposts=$wpdb->get_results($query);
+	 
+	 $rowsposts8 = '';
+	 $postsbycat = '';
+	 if(isset($_POST["iframecatid"])){
+	 	  $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."term_relationships where term_taxonomy_id = %d order by object_id ASC",$_POST["iframecatid"]);
+		$rowsposts8=$wpdb->get_results($query);
+
+
+	 
+
+			   foreach($rowsposts8 as $rowsposts13){
+	 $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."posts where post_type = 'post' and post_status = 'publish' and ID = %d  order by ID ASC",$rowsposts13->object_id);
+			   $rowsposts1=$wpdb->get_results($query);
+			   $postsbycat = $rowsposts1;
+			   
+	 }
+	 }
+	
+	$query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."videogallery_huge_itslider_sliders WHERE id= %d",$id);
+	$rowslider=$wpdb->get_row($query);
+    Html_editvideogallery($ord_elem, $count_ord, $images, $row, $cat_row, $rowim, $rowsld, $paramssld, $rowsposts, $rowsposts8, $postsbycat, $rowslider);
+  }
+  
+function add_videogallery()
+{
+	global $wpdb;
+	
+	$query="SELECT name,ordering FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE sl_width=0 ORDER BY `ordering`";
+	$ord_elem=$wpdb->get_results($query); ///////ordering elements list
+	$cat_row=$wpdb->get_results("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries where sl_width=0");
+	$cat_row=open_cat_in_tree($cat_row);
+	
+	$table_name = $wpdb->prefix . "huge_it_videogallery_galleries";
+    $sql_2 = "
+INSERT INTO 
+
+`" . $table_name . "` ( `name`, `sl_height`, `sl_width`, `pause_on_hover`, `videogallery_list_effects_s`, `description`, `param`, `ordering`, `published`, `huge_it_sl_effects`) VALUES
+( 'New Video Gallery', '375', '600', 'on', 'cubeH', '4000', '1000', '1', '300', '4')";
+
+    $wpdb->query($sql_huge_it_videogallery_galleries);
+
+      $wpdb->query($sql_2);
+
+   $query="SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries order by id ASC";
+			   $rowsldcc=$wpdb->get_results($query);
+			   $last_key = key( array_slice( $rowsldcc, -1, 1, TRUE ) );
+			   
+			   
+	foreach($rowsldcc as $key=>$rowsldccs){
+		if($last_key == $key){
+			header('Location: admin.php?page=videogallerys_huge_it_videogallery&id='.$rowsldccs->id.'&task=apply');
+		}
+	}
+	
+	html_add_videogallery($ord_elem, $cat_row);
+	
+}
+
+
+
+
+function videogallery_video($id)
+{
+	  global $wpdb;
+
+	  if(isset($_POST["huge_it_add_video_input"])){
+	  if($_POST["huge_it_add_video_input"] != ''){
+	  $table_name = $wpdb->prefix . "huge_it_videogallery_videos";
+	  
+
+	  $sql_video = "INSERT INTO 
+`" . $table_name . "` ( `name`, `videogallery_id`, `description`, `image_url`, `sl_url`, `sl_type`, `link_target`, `ordering`, `published`, `published_in_sl_width`) VALUES 
+( '".$_POST["show_title"]."', '".$id."', '".$_POST["show_description"]."', '".$_POST["huge_it_add_video_input"]."', '".$_POST["show_url"]."', 'video', 'on', '0', '1', '1' )";
+	
+
+	  $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id= %d",$id);
+	   $row=$wpdb->get_row($query);
+	    $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_videos where videogallery_id = %d order by id ASC", $row->id);
+			   $rowplusorder=$wpdb->get_results($query);
+			   
+			   foreach ($rowplusorder as $key=>$rowplusorders){
+$rowplusorderspl=$rowplusorders->ordering+1;
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET ordering = '".$rowplusorderspl."' WHERE id = %d ", $rowplusorders->id));
+}
+	   $wpdb->query($sql_video);
+	  }
+	  }
+	 
+    Html_videogallery_video();
+}
+
+
+function removevideogallery($id)
+{
+	global $wpdb;
+	 $sql_remov_tag=$wpdb->prepare("DELETE FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id = %d", $id);
+ if(!$wpdb->query($sql_remov_tag))
+ {
+	  ?>
+	  <div id="message" class="error"><p>videogallery Not Deleted</p></div>
+      <?php
+ }
+ else{
+ ?>
+ <div class="updated"><p><strong><?php _e('Item Deleted.' ); ?></strong></p></div>
+ <?php
+ }
+}
+
+function apply_cat($id)
+{	
+
+		 global $wpdb;
+		 if(!is_numeric($id)){
+			 echo 'insert numerc id';
+		 	return '';
+		 }
+		 if(!(isset($_POST['sl_width']) && isset($_POST["name"]) ))
+		 {
+			echo '';
+		 }
+		 $cat_row=$wpdb->get_results($wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id!= %d ", $id));
+		 $corent_ord=$wpdb->get_var($wpdb->prepare('SELECT `ordering` FROM '.$wpdb->prefix.'huge_it_videogallery_galleries WHERE id = %d AND sl_width=%d',$id,$_POST['sl_width']));
+		 $max_ord=$wpdb->get_var('SELECT MAX(ordering) FROM '.$wpdb->prefix.'huge_it_videogallery_galleries');
+	
+            $query=$wpdb->prepare("SELECT sl_width FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id = %d", $id);
+	        $id_bef=$wpdb->get_var($query);
+      
+	if(isset($_POST["content"])){
+	$script_cat = preg_replace('#<script(.*?)>(.*?)</script>#is', '', stripslashes($_POST["content"]));
+	}
+			if($_POST["name"] != ''){
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  name = '".$_POST["name"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  sl_width = '".$_POST["sl_width"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  sl_height = '".$_POST["sl_height"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  pause_on_hover = '".$_POST["pause_on_hover"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  videogallery_list_effects_s = '".$_POST["videogallery_list_effects_s"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  description = '".$_POST["sl_pausetime"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  param = '".$_POST["sl_changespeed"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  sl_position = '".$_POST["sl_position"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  huge_it_sl_effects = '".$_POST["huge_it_sl_effects"]."'  WHERE id = '".$id."' ");
+			$wpdb->query("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  ordering = '1'  WHERE id = '".$id."' ");
+			}
+		
+	$query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_galleries WHERE id = %d", $id);
+	   $row=$wpdb->get_row($query);
+
+			    $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_videos where videogallery_id = %d order by id ASC", $row->id);
+			   $rowim=$wpdb->get_results($query);
+			   
+			   foreach ($rowim as $key=>$rowimages){
+if(isset($_POST["order_by_".$rowimages->id.""])){
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  ordering = '".$_POST["order_by_".$rowimages->id.""]."'  WHERE ID = %d ", $rowimages->id));
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  link_target = '".$_POST["sl_link_target".$rowimages->id.""]."'  WHERE ID = %d ", $rowimages->id));
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  sl_url = '".$_POST["sl_url".$rowimages->id.""]."' WHERE ID = %d ", $rowimages->id));
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  name = '".$_POST["titleimage".$rowimages->id.""]."'  WHERE ID = %d ", $rowimages->id));
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  description = '".$_POST["im_description".$rowimages->id.""]."'  WHERE ID = %d ", $rowimages->id));
+$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  image_url = '".$_POST["imagess".$rowimages->id.""]."'  WHERE ID = %d ", $rowimages->id));
+}
+}
+
+if (isset($_POST['params'])) {
+      $params = $_POST['params'];
+      foreach ($params as $key => $value) {
+          $wpdb->update($wpdb->prefix . 'huge_it_videogallery_params',
+              array('value' => $value),
+              array('name' => $key),
+              array('%s')
+          );
+      }
+     
+    }
+	
+		
+	   
+	   if($_POST["imagess"] != ''){
+				   		   $query=$wpdb->prepare("SELECT * FROM ".$wpdb->prefix."huge_it_videogallery_videos where videogallery_id = %d order by id ASC", $row->id);
+			   $rowim=$wpdb->get_results($query);
+	  foreach ($rowim as $key=>$rowimages){
+	  $orderingplus = $rowimages->ordering+1;
+	  $wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_videos SET  ordering = %d  WHERE ID = %d ", $orderingplus, $rowimages->id));
+	  }
+	$table_name = $wpdb->prefix . "huge_it_videogallery_videos";
+	$imagesnewuploader = explode(";;;", $_POST["imagess"]);
+	array_pop($imagesnewuploader);
+	foreach($imagesnewuploader as $imagesnewupload){
+	
+    $sql_2 = "
+INSERT INTO 
+
+`" . $table_name . "` ( `name`, `videogallery_id`, `description`, `image_url`, `sl_url`, `sl_type`, `link_target`, `ordering`, `published`, `published_in_sl_width`) VALUES
+( '', '".$row->id."', '', '".$imagesnewupload."', '', 'image', 'on', 'par_TV', 2, '1' )";
+
+      $wpdb->query($sql_2);
+		}	
+	   }
+	   
+	if(isset($_POST["posthuge-it-description-length"])){
+	 $wpdb->query($wpdb->prepare("UPDATE ".$wpdb->prefix."huge_it_videogallery_galleries SET  published = %d WHERE id = %d ", $_POST["posthuge-it-description-length"], $_GET['id']));
+}
+	?>
+	<div class="updated"><p><strong><?php _e('Item Saved'); ?></strong></p></div>
+	<?php
+	
+    return true;
+	
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Video Gallery ===
+Contributors: Huge-IT
+Donate link: http://huge-it.com/video-gallery/
+Tags: video gallery, gallery, video, youtube, vimeo, wp gallery,
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Video Gallery plugin was created and specifically designed to show your video files in unusual splendid ways.
+
+== Description ==
+
+### Wordpress Video Gallery
+
+Video Gallery plugin was created and specifically designed to show your video files in unusual splendid ways. It has 5 good-looking views. Each are made in different taste so that you can choose any of them, according to the style of your website. Upload the video in your Video Gallery, wright a title, add description and give some link. On the left panel you can find 5 unrepeatable views, it has a view to show your videos with it’s information next to it in a beautiful popup, or the same view can be slide, in the second view, then you can choose to demonstrate  the videos in lightbox, or only slide the videos, in the next view, set the way to slide and have fun, the last view is to show your videos in a little box which can be opened in lightbox and be slide at a time. So the choice are huge. You can provide all the information as from uploader panel, as after inserting in the Video Gallery panel. Each of the view has changeable options in ‘General Options’ section of Video Gallery. Every detail is allowed to be change so that our users be satisfy of the results they get by using Video Gallery plugin. And everything are so easy to use that even your child can manage it. Make the Video shows. And demonstrate the content of your website in more kindly and pleasant way.  The  Video Gallery allows you to add video from 2 kind of sources, it is Vimeo and Youtube. From time to time we will make all kind of updates, and add many other even better view to our Video Gallery, so that to be competitive among the other Video plugins in WordPress. So we have an aim to be the best Video Gallery and we will achieve our goal. Now it time for you to install and just try how nice it works!
