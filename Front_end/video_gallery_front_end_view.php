283ce38e0cab3ed8f0a803d68aa8a688dd39694a
Huge-IT <Huge-IT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 08:27:12 +0000

    Bugs have been fixed on Video Gallery.
    git-svn-id: https://plugins.svn.wordpress.org/gallery-video/trunk@1080688 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Front_end/video_gallery_front_end_view.php
+++ Front_end/video_gallery_front_end_view.php
@@ -55,8 +55,10 @@ function front_end_videogallery($images, $paramssld, $videogallery)
 	var lightbox_initialWidth = '<?php echo $paramssld['light_box_initialwidth'];?>';
 	var lightbox_initialHeight = '<?php echo $paramssld['light_box_initialheight'];?>';
 	
-        var lightbox_maxWidth = '';
-        var lightbox_maxHeight = '';
+        var maxwidth=jQuery(window).width();
+        if(maxwidth > <?php echo $paramssld['light_box_width']; ?> ){ maxwidth= <?php echo $paramssld['light_box_width']; ?> ; }
+        var lightbox_maxWidth = <?php if($paramssld['light_box_size_fix'] == 'true'){ echo '"100%"'; } else { echo 'maxwidth'; } ?>;
+        var lightbox_maxHeight = <?php if($paramssld['light_box_size_fix'] == 'true'){ echo '"100%"'; } else { echo $paramssld['light_box_height']; } ?>;
         
 	var lightbox_slideshow = <?php echo $paramssld['light_box_slideshow'];?>;
 	var lightbox_slideshowSpeed = <?php echo $paramssld['light_box_slideshowspeed'];?>;
